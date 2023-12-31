7e696f9fc380e2d740b6564ce6ad413b6003ec8b
mechairoi <ttsujikawa@gmail.com>
2023-12-31 23:53:06 +0900

    copy qwik-react 0.5.0 from https://github.com/BuilderIO/qwik

---
+++ packages/qwik-solid/src/examples/app.tsx
@@ -0,0 +1,11 @@
+/** @jsxImportSource react */
+
+import { qwikify$ } from '../react/qwikify';
+
+export const App = qwikify$(() => {
+  return (
+    <>
+      <div>hola</div>
+    </>
+  );
+});
---
+++ packages/qwik-solid/src/vite.ts
@@ -0,0 +1,24 @@
+export function qwikReact(): any {
+  const OPTIMIZE_DEPS = [
+    'react',
+    'react-dom',
+    'react-dom/client',
+    'react/jsx-runtime',
+    'react/jsx-dev-runtime',
+  ];
+  const DEDUPE = ['react', 'react-dom'];
+
+  return {
+    name: 'vite-plugin-qwik-react',
+    config() {
+      return {
+        resolve: {
+          dedupe: DEDUPE,
+        },
+        optimizeDeps: {
+          include: OPTIMIZE_DEPS,
+        },
+      };
+    },
+  };
+}
