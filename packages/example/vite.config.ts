c65a28989fec81350d4a333d7ef618be533d04c5
mechairoi <ttsujikawa@gmail.com>
2024-01-01 09:35:22 +0900

    qwik-city with qwik-solid example

--- packages/example/vite.config.ts
+++ packages/example/vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
+import { qwikSolid } from "@mechairoi/qwik-solid/vite";
+import solid from 'vite-plugin-solid'
+
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [solid({include: './src/integrations/solid/**', ssr: true}), qwikCity(), qwikVite(), tsconfigPaths(), qwikSolid()],
     dev: {
       headers: {
         "Cache-Control": "public, max-age=0",
