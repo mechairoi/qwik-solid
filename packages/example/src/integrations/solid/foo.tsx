d2542559639288d9d050be233282573976990d3e
mechairoi <ttsujikawa@gmail.com>
2024-01-05 19:44:18 +0900

    rm unused file

--- packages/example/src/integrations/solid/foo.tsx
+++
@@ -1,9 +0,0 @@
-import { qwikify$ } from "@mechairoi/qwik-solid";
-// import App from "@try-qwik-solid/solid-example";
-import App from "./App";
-
-export const SolidApp = qwikify$(App);
-export const SolidAppHover = qwikify$(App, { eagerness: "hover" });
-
-// export const MUIButton = qwikify$(Button);
-// export const MUISlider = qwikify$(Slider, { eagerness: "hover" });
